4efa07e732e026c0e8c7c27149744db82f0c73c2
Taras Danyliuk <t.danyliuk@relevant.software>
2022-03-05 14:33:56 +0200

    add lasts for row

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,46 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from "react";
+import { formatDuration, intervalToDuration } from 'date-fns'
+
+
+const start = new Date("2022-02-24T02:00:00.000Z");
 
 export default function Home() {
+  const [duration, setDuration] = useState(formatDuration(intervalToDuration({
+    start: start,
+    end: new Date()
+  })));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDuration(formatDuration(intervalToDuration({
+        start: start,
+        end: new Date()
+      })))
+    }, 1000)
+
+    return () => {
+      clearTimeout(intervalId)
+    }
+  }, []);
+
   return (
-    <div className="h-100 flex" style={{ background: 'pink' }}>
+    <div className="h-100">
       <Head>
         <title>No Fly Zone</title>
         <meta name="description" content="Close the sky above Ukraine" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className="flex-column align-center">
+        <h1>The war in Ukraine lasts for:</h1>
+        <h2>{duration}</h2>
+      </div>
+
+      <div className="flex-column align-center">
+        <h3>Casualties by that time:</h3>
+      </div>
     </div>
   )
 }
