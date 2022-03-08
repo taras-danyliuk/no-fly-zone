6d569e8b274232e86cf9162e4bc2a8afd219deb7
Taras Danyliuk <t.danyliuk@relevant.software>
2022-03-08 14:25:54 +0200

    feat: new lines

--- slides/share/Share.js
+++ slides/share/Share.js
@@ -24,7 +24,9 @@ const Share = () => {
   return (
     <Slide hideHashtag justify="center">
       <Title>
-        Stop this madness! Spread the word!
+        Stop this madness!
+        <br/>
+        Spread the word!
         <br/>
         <span className="text-red">Save Thousands of lives!</span>
       </Title>
